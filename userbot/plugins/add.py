b4b29e5c997d0a77bd305bda5df3de418719c1a6
cyrsbly <65646317+cyrsbly@users.noreply.github.com>
2020-10-20 14:52:00 +0800

    Update add.py

--- userbot/plugins/add.py
+++ userbot/plugins/add.py
@@ -36,7 +36,7 @@ async def _(event):
                     )
                 except Exception as e:
                     await event.reply(str(e))
-            await event.edit("Invited Successfully")
+            await event.edit("👌")
         else:
             # https://lonamiwebs.github.io/Telethon/methods/channels/invite_to_channel.html
             for user_id in to_add_users.split(" "):
@@ -48,4 +48,4 @@ async def _(event):
                     )
                 except Exception as e:
                     await event.reply(str(e))
-            await edit_or_reply(event, "Invited Successfully")
+            await edit_or_reply(event, "👌")
