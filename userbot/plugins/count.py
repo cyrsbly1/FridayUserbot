f983d33cd6bd7a8bf3d3c56fb4c3461f660de3ba
cyrsbly <65646317+cyrsbly@users.noreply.github.com>
2020-11-06 07:44:10 +0800

    Update

--- userbot/plugins/count.py
+++ userbot/plugins/count.py
@@ -56,7 +56,7 @@ async def stats(
     event: NewMessage.Event,
 ) -> None:  # pylint: disable = R0912, R0914, R0915
     """Command to get stats about the account"""
-    await event.edit("`Collecting stats, Wait Master`")
+    await event.edit("`Collecting stats... Please wait.`")
     start_time = time.time()
     private_chats = 0
     bots = 0
